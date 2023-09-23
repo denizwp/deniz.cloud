a21b9ada63076e5d097198d9b6a0ebee870265cf
deniz <85676222+denizwp@users.noreply.github.com>
2023-09-23 17:37:37 +0300

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -14,5 +14,10 @@ export const socials: SocialInfo[] = [
 		name: "Spotify",
 		image: "/images/spotify.png",
 		url: "https://open.spotify.com/user/szviltwriiiu23qzw5uriwtw7?si=ac87b62eab114042"
+	},
+	{
+		name: "Buy Me a Coffee",
+		image: "/images/buymeacoffee.png",
+		url: "https://www.buymeacoffee.com/denizaight"
 	}
 ];
