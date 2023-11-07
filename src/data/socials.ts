dacb4fd7d65fd86b74557fc1672b769684c8a0d2
deniz <85676222+denizistaken@users.noreply.github.com>
2023-11-07 19:59:48 +0300

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -8,7 +8,7 @@ export const socials: SocialInfo[] = [
 	{
 		name: "GitHub",
 		image: "/images/github.png",
-		url: "https://github.com/denizaight"
+		url: "https://github.com/denizistaken"
 	},
 	{
 		name: "Figma",
