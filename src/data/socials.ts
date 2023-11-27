dbf8d9a82e44fc5e0d28223f67ca1a47bd6ef644
deniz <85676222+denizistaken@users.noreply.github.com>
2023-11-27 23:17:34 +0300

    Update socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -11,6 +11,11 @@ export const socials: SocialInfo[] = [
 		url: "https://github.com/denizistaken"
 	},
 	{
+		name: "Instagram",
+		image: "/images/instagram.png",
+		url: "https://instagram.com/forealdeniz"
+	},
+	{
 		name: "Spotify",
 		image: "/images/spotify.png",
 		url: "https://open.spotify.com/user/szviltwriiiu23qzw5uriwtw7?si=ac87b62eab114042"
