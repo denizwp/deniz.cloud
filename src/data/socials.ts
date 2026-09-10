ea76e16f346420dcf56c48d05cb3f2e7d1bd6ecf
denizwp <denizwp01@gmail.com>
2026-09-10 19:39:14 +0300

    Add Buy Me a Coffee to the socials row

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -20,6 +20,11 @@ export const socials: SocialInfo[] = [
 		image: "/images/spotify.png",
 		url: "https://open.spotify.com/user/szviltwriiiu23qzw5uriwtw7?si=ac87b62eab114042"
 	},
+	{
+		name: "Buy Me a Coffee",
+		image: "/images/buymeacoffee.png",
+		url: "https://buymeacoffee.com/denizwp"
+	},
 	// {
 	// 	name: "Instagram",
 	// 	image: "/images/instagram.png",
